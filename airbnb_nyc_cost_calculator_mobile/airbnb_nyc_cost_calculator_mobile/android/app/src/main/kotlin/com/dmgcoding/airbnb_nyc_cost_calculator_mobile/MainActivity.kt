9d1c33c8699a2a102fdf3cd5a02c54df9e2e7c1a
dmgcoding <dmgcoding@gmail.com>
2023-06-22 17:01:17 +0530

    all apps added

---
+++ airbnb_nyc_cost_calculator_mobile/airbnb_nyc_cost_calculator_mobile/android/app/src/main/kotlin/com/dmgcoding/airbnb_nyc_cost_calculator_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dmgcoding.airbnb_nyc_cost_calculator_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ airbnb_nyc_cost_calculator_mobile/airbnb_nyc_cost_calculator_mobile/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:airbnb_cost_calculator/uis/screens/welcome.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Airbnb Cost Calculator',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Welcome(),
+    );
+  }
+}
